c07e5731cff67bfade9318651b7b7d6ceca309c2
eryisan <525559856@qq.com>
2022-07-20 01:46:22 +0800

    @blog0.4.2

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import blog from "https://deno.land/x/blog@0.4.1/blog.tsx";
+import blog from "https://deno.land/x/blog@0.4.2/blog.tsx";
 
 blog({
   title: "贰壹叁",
@@ -11,17 +11,5 @@ blog({
     { title: "Email", url: "mailto:eryisan213@qq.com" },
     { title: "GitHub", url: "https://github.com/eryisan" },
     { title: "Twitter", url: "https://twitter.com/IamNullUser" },
-  ],
-  // middlewares: [
-
-    // If you want to set up Google Analytics, paste your GA key here.
-    // ga("UA-XXXXXXXX-X"),
-
-    // If you want to provide some redirections, you can specify them here,
-    // pathname specified in a key will redirect to pathname in the value.
-    // redirects({
-    //  "/hello_world.html": "/hello_world",
-    // }),
-
-  // ]
+  ]
 });
